d834b2bcaae3843c69fe291cd71f654601f3d778
Yukinori Yamazoe <drocon11@gmail.com>
2014-05-31 13:43:31 +0900

    Add libmsvcrt.a to linker option of MSVC

--- build-L-SMASH-Works-ffmpeg-AviSynth.sh
+++ build-L-SMASH-Works-ffmpeg-AviSynth.sh
@@ -17,7 +17,7 @@ git pull -v --progress
 cd AviSynth
 cat <<__END_OF_TEXT__ >"LSMASHSourceVCX.vcxproj.bat"
 set CL=/I..\..\..\..\..\include /I..\..\msinttypes
-set LINK="libpthread.a" "libiconv.a" "libswresample.a" /LIBPATH:..\..\..\..\..\i686-w64-mingw32\lib /LIBPATH:..\..\..\..\..\lib\gcc\i686-w64-mingw32\4.8.2 /LIBPATH:..\..\bzip2 /LIBPATH:..\..\..\..\..\lib
+set LINK="libpthread.a" "libiconv.a" "libswresample.a" "libmsvcrt.a" /LIBPATH:..\..\..\..\..\i686-w64-mingw32\lib /LIBPATH:..\..\..\..\..\lib\gcc\i686-w64-mingw32\4.8.2 /LIBPATH:..\..\bzip2 /LIBPATH:..\..\..\..\..\lib
 @for /d %%1 in (%SystemRoot%\Microsoft.NET\Framework\v*) do @if exist "%%~1\msbuild.exe" set "MSBUILD=%%~1\msbuild.exe"
 "%MSBUILD%" LSMASHSourceVCX.vcxproj /target:Rebuild /property:Configuration=Release;Platform=Win32;PlatformToolset=v100
 __END_OF_TEXT__
